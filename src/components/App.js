f8e119f3e86848b093f87e68e45cd7c4440c7a93
dharminder <144053986+DharminderPal@users.noreply.github.com>
2025-04-09 12:49:56 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,47 @@
-
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+   
+    fetch('https://dummyjson.com/products')
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`Error: ${res.status}`);
+        }
+        return res.json();
+      })
+      .then((result) => {
+        setData(result); 
+      })
+      .catch((err) => {
+        setError(err.message); 
+      })
+      .finally(() => {
+        setIsLoading(false); 
+      });
+  }, []); 
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {isLoading && <p>Loading...</p>}
+      {!!error && <p>An error occurred: {error}</p>}
+      {!!data && (
+        <>
+          <h1>Data Fetched from API</h1>
+          <pre>{JSON.stringify(data, null, 2)}</pre>
+        </>
+      )}
+      {!data && !isLoading && !error && <p>No data found</p>}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
+
+
